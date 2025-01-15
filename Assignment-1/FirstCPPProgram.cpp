f253cd7663e972d51986cfc5bc63c0cff49dc596
zdhaxela <alexa.hernandez9019@gmail.com>
2025-01-14 23:15:37 -0600

    Initial commit first assignment

---
+++ Assignment-1/FirstCPPProgram.cpp
@@ -0,0 +1,12 @@
+// library that has access to the functions that has cout & cin
+#include <iostream>
+ // folder to avoid confusion
+using namespace std;
+// returns integers and where the program starts
+int main ()
+{
+    //  writes to the console /line ends
+    cout << "Coke Zero Bottle" << endl;
+    // return succes
+    return 0;
+}
---
+++ Assignment-1/Firstandahalf.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <iostream>
+
+
+using namespace std;
+
+int main ()
+
+{
+    string coke;
+    getline(cin, coke);
+    cout << coke;
+
+    
+
+    return 0;
+
+}
